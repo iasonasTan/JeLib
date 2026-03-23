df2e5e05e72aefdf53e2cb6f5011560f096baa1c
Jason <iasonas.tan@gmail.com>
2026-03-23 13:31:31 +0200

    Added doc to MapBuilder

--- jelib-core/src/main/java/com/je/core/MapBuilder.java
+++ jelib-core/src/main/java/com/je/core/MapBuilder.java
@@ -3,17 +3,48 @@ package com.je.core;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class used to build a {@link Map} with 
+ * cleaner and more readable code.
+ */
 public final class MapBuilder<K, V> {
-    private final Map<K, V> mMap=new HashMap<>();
+    /**
+     * Implementation of map that holds the data.
+     */
+    private final Map<K, V> mMap;
 
+    /**
+     * No-arg constructor, initializes with {@link HashMap}.
+     * @see MapBuilder(Map)
+     */
     public MapBuilder() {
+        this(new HashMap<K, V>());
+    }
+    
+    /**
+     * Constructor that takes instance that will 
+     * be used to store and returned.
+     */
+    public MapBuilder(Map<K, V> impl) {
+        mMap = impl;
     }
 
+    /**
+     * Puts a value in map that's going to be returned.
+     * @param k key to add
+     * @param v value to add
+     * @return returns this so chain calls are possible
+     */
     public MapBuilder<K, V> put(K k, V v) {
         mMap.put(k, v);
         return this;
     }
 
+    /**
+     * Returns instance of the put data.
+     * @return {@link Map} implementation with put values.
+     * @see #put(K,V)
+     */
     public Map<K, V> build() {
         return mMap;
     }
